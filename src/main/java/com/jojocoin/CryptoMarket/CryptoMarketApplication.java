7be6f7ac0250a989e659202c9b13a5c248d91310
Andre Mapa <andreomapa01@gamil.com>
2022-07-28 22:27:14 -0300

    First Commit - Base project done!

---
+++ src/main/java/com/jojocoin/CryptoMarket/CryptoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jojocoin.CryptoMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojocoin/CryptoMarket/CryptoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojocoin.CryptoMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
